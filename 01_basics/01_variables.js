a8826f8133b63a711d89120175f2a9eb810011c3
aishwarya-aishw <119443890+aishwarya-aishw@users.noreply.github.com>
2024-12-29 06:17:15 +0000

    basics on variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const userName = "Aishwarya"
+let userId = 'aishwarya@gmail.com'
+var userPassword = "123456"
+randomVar = "aishwarya@1234"
+let notDefined;
+
+
+console.log(userName);
+
+console.table([userPassword,userId, userPassword, randomVar, notDefined]);
+
+
+// Always use let as a changable variable
+// can use console.table 
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+/*
+number
+string
+float
+bigInt
+boolean
+null => empty value/standalone value doesnt mean its zero
+undefined
+symbol => useful to find uniqueness
+
+object
+
+*/
+console.log(typeof null);
+console.log(typeof undefined);
